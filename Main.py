d9cae4ce06c439aa5457f74dc97340e43ece20d0
AndrewTheeFirst <pattona@southern.edu>
2024-11-01 12:49:13 -0400

    first commit

---
+++ Main.py
@@ -0,0 +1,29 @@
+from PhaseTen import *
+from os import system
+
+#Deals cards to players and puts them in a list
+
+numPlayers = int(input('How many players (2-6): '))
+print()
+
+myGame = PhaseTen(numPlayers)
+
+playerList = [Player(hand) for hand in myGame.deal()] 
+
+#access player objects via playerList
+
+while True:
+    for player in playerList:
+        player.Turn(myGame)
+        input('press any key to end your turn...')
+        system('cls')                             #wipes screen so players have privacy 
+        input('enter any key to continue...\n') 
+
+
+while True:
+        player.Turn(myGame)
+        input('press any key to end your turn...')
+        system('cls')                             #wipes screen so players have privacy 
+        input('enter any key to continue...\n') 
+
+
