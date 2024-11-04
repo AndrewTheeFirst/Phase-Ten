65fac9e5f92a0808e9c6462ebd21cc93ce391ed9
ANDREWIDEAPAD\03dre <pattona@southern.edu>
2024-11-03 20:04:57 -0500

    Implemented Hand sorts, isSet, and isRun

--- PhaseTen.py
+++ PhaseTen.py
@@ -3,16 +3,15 @@
 # Started Refactoring 2024-11-1
 # Further Refactoring 2024-11-2
 
-from constants import *
+from constants import Faces, Colors, CARD_RENDERS, PRINT_COLOR, CARD_TOP_BOT, CLEAR
 from random import shuffle as _shuffle
 
-
 class Card:
+    '''Represents phaseten phasecards'''
     def __init__(self, face: Faces, color: Colors):
         self.face = face
         self.color = color
         self._repr = color.name.lower()[0] + str(face.value) if face not in [Faces.WILD, Faces.SKIP] else face.name.lower()[0]
-
         
     def __str__(self):
         card = ""
@@ -32,7 +31,7 @@ class Card:
         return self.face.value
 
 class Stack:
-    '''Represents a stack of cards'''
+    '''Represents a generic stack of cards (superclass)'''
     def __init__(self):
         self.cards: list[Card] = []
 
@@ -49,33 +48,14 @@ class Stack:
         self.cards = []
 
     def is_empty(self):
-        return len(self.cards) == 0
+        return self.size() == 0
+    
+    def size(self):
+        return len(self.cards)
 
     def __str__(self):
         '''subclasses will override this method '''
-        pass
-
-class Hand(Stack):
-    def __str__(self):
-        hand_builder = ""
-        card_lines = [str(self.cards[index]).split('\n') for index in range(len(self.cards))]
-        card_height = len(card_lines[0])
-        num_cards = len(card_lines)
-        for line in range(card_height):
-            for card in range(num_cards):
-                hand_builder += card_lines[card][line] + "  "
-            hand_builder += '\n'
-        return hand_builder
-    
-    def pop(self):
-        while True:
-            card_repr = input("Which card would you like to drop?: ")
-            for index in range(len(self.cards)):
-                current_repr =  self.cards[index]._repr
-                if card_repr == current_repr:
-                    return self.cards.pop(index)
-            print("Card not found. (ex. g3 is Green 4; w is Wild)")
-            
+        pass          
 
 class Discard(Stack):
     '''Discard is a Stack'''
@@ -128,17 +108,94 @@ class Pickup(Stack):
                 card_builder += cards[0][index] + "  " + cards[1][index] + '\n'
         return card_builder
 
+class Hand(Stack):
+    '''Represents a player-owned stack of cards'''
+    def __str__(self):
+        hand_builder = ""
+        num_cards = self.size()
+        card_lines = [str(self.cards[index]).split('\n') for index in range(num_cards)]
+        card_height = len(card_lines[0])
+        for line in range(card_height):
+            for card_index in range(num_cards):
+                hand_builder += card_lines[card_index][line] + "  "
+            hand_builder += '\n'
+        return hand_builder
+    
+    def pop(self):
+        while True:
+            card_repr = input("Which card would you like to drop?: ")
+            for index in range(self.size()):
+                current_repr = self.cards[index]._repr
+                if card_repr == current_repr:
+                    return self.cards.pop(index)
+            print("Card not found. (ex. g3 is Green 4; w is Wild)")
+
+    def face_sort(self):
+        self.cards = sorted(self.cards, key = lambda card: card.value()) # may modify
+
+    def color_sort(self):
+        self.cards = sorted(self.cards, key = lambda card: card.color.value) # may modify
+
+class Phase(Hand):
+    def __init__(self):
+        super().__init__()
+        self.num_cards = 0
+        self.num_wilds = 0
+        self.has_skip = False    
+
+    def push(self, card: Card):
+        super().push(card)
+        if card.face == Faces.WILD:
+            self.num_wilds += 1
+        elif card.face == Faces.SKIP:
+            self.has_skip = True
+        self.num_cards += 1
+    
+    def isSet(self, size: int, set_type: str):
+        '''Returns true if cards creates a set of specified size and set_type (ie. 'face' or 'color')'''
+        if self.num_cards < size:
+            return False
+        elif set_type == "face": # Goes through all cards except wilds and checks 
+            return all([(self.cards[card1_index].face == self.cards[card2_index].face)\
+                            for card1_index in range(self.num_cards - 1 - self.num_wilds)\
+                            for card2_index in range(card1_index, self.num_cards - self.num_wilds)])\
+                            and not self.has_skip
+        elif set_type == "color":
+            return all([(self.cards[card1_index].color == self.cards[card2_index].color)\
+                            for card1_index in range(self.num_cards - 1 - self.num_wilds)\
+                            for card2_index in range(card1_index, self.num_cards - self.num_wilds)])\
+                            and not self.has_skip
+    
+    def isRun(self, size: int):
+        self.face_sort()
+        num_wilds = self.num_wilds
+        if self.num_cards < size:
+            return False
+        card_values = [card.value() for card in self.cards]
+        last_value, expected_value = card_values[0], card_values[0] + 1
+        for card_index in range(1, self.num_cards - num_wilds): # Goes through all cards except wilds
+            curr_value = card_values[card_index]
+            if last_value == curr_value: # cannot have duplicates
+                return False
+            elif expected_value != curr_value:
+                num_wilds -= curr_value - expected_value # calculates the number of cards missing from inbetween
+            last_value, expected_value = curr_value, curr_value + 1
+        return num_wilds >= 0 and not self.has_skip
+    
 class Player:
     # Player Has a Phase and a Hand
     def __init__(self):
-        self.out = False
         self.hand = Hand()
-
-class Phase:
-    # Phase Template
-    def __init__(self):
-        pass
+        self.phase = Phase()
+        self.out = False
+        self.points = 0
 
 if __name__ == "__main__":
-    c1 = Card(Faces.WILD, Colors.ANY)
-    print(c1._repr)
+    p1 = Phase()
+    p1.push(Card(Faces.ONE, Colors.YELLOW))
+    p1.push(Card(Faces.WILD, Colors.ANY))
+    p1.push(Card(Faces.WILD, Colors.ANY))
+    p1.push(Card(Faces.FIVE, Colors.RED))
+    p1.push(Card(Faces.WILD, Colors.ANY))
+    p1.push(Card(Faces.SEVEN, Colors.ANY))
+    print(p1.isRun(5))
--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from PhaseTen import Player, Pickup
+from PhaseTen import Player, Pickup, Phase
 from itertools import cycle
 from os import system, name as os_name
 
@@ -26,32 +26,39 @@ class Game:
             num_players = intin("How many players (2-6): ", (2, 6))
         self.players = [Player() for _ in range(num_players)]
         self.deck = Pickup()
+        self.phases: list[Phase] = []
         self.discard = self.deck.discard
         self.game_running = True
         self.round_over = False
     
-    def show(self, player: Player):
-        clear()
-        print(player.hand)
-        print(self.deck)
-        print()
-
     def deal(self):
         for _ in range(10):
             for player in self.players:
                 player.hand.push(self.deck.pop())
         self.discard.push(self.deck.pop())
 
-    def set_turn(self, player: Player):
+    def show_table(self, curr_player: Player):
+        clear()
+        for phase in self.phases:
+            pass
+        print(self.deck, end = "\n\n")
+        print(curr_player.hand)
 
-        self.show(player)
-        choice = intin("Draw from Discard(1) or from Deck(2): ", (1, 2))
-        
-        if choice == 1: # Player picks up a card
-            player.hand.push(self.discard.pop())
-        else:
-            player.hand.push(self.deck.pop())
-        self.show(player)
+    def player_turn(self, player: Player):
+        self.show_table(player)
+        choice = 3
+        while choice == 3:
+            choice = intin("Draw from Discard(1), from Deck(2), or Sort(3): ", (1, 3))
+            if choice == 1: # Player picks up a card
+                player.hand.push(self.discard.pop())
+            elif choice == 2:
+                player.hand.push(self.deck.pop())
+            else:
+                if intin("Would you like to sort by Face(1) or Color(2): ", (1, 2)) == 1:
+                    player.hand.face_sort()
+                else:
+                    player.hand.color_sort()
+            self.show_table(player)
 
         # TODO Can player complete phase ?
 
@@ -63,7 +70,7 @@ class Game:
 
         # TODO Check if player is has no more cards (here too)
 
-        self.show(player)
+        self.show_table(player)
 
 
         input('press any key to end your turn...')
@@ -77,11 +84,11 @@ class Game:
             self.deal()
             while not self.round_over:
                 input('enter any key to continue...\n')
-                self.set_turn(next(turn))
-
+                self.player_turn(next(turn))
             for player in self.players:
                 player.hand.clear()
-                # TODO Increment phase of all out players
+                if player.out:
+                    player.phase += 1
                 player.out = False
             
 if __name__ == "__main__":
